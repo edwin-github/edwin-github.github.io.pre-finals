b4f5dd18e3fb618aa5c4e7354f5ed8eeca1d9b19
edwin-github <edwin-github@users.noreply.github.com>
2015-03-16 11:13:29 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,15 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    /*
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      */
+      var dx = determineDx(.randomPizzaContainer, size);
+      var newwidth = (.randomPizzaContainer.offsetWidth + dx) + 'px';
+      .randomPizzaContainer.style.width = newwidth;
     }
   }
 

8b1559f406a5059e829c59eb8b94a2c132baa4ee
edwin-github <edwin-github@users.noreply.github.com>
2015-03-16 19:20:29 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -534,7 +534,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza_med.png";
+    elem.src = "images/pizza.png";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);

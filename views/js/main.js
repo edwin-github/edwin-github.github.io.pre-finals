b7b6c3abd0775a2d418d0d9d30aa390caa0d8442
edwin-github <edwin-github@users.noreply.github.com>
2015-03-18 11:34:00 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -411,12 +411,12 @@ return dx;
 function changePizzaSizes(size) {
 var dx = determineDx(document.querySelector(".randomPizzaContainer"), size);
 var newwidth = (document.querySelector(".randomPizzaContainer").offsetWidth + dx) + 'px';
-document.querySelector(".randomPizzaContainer").style.width = newwidth;
-//for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+//document.querySelector(".randomPizzaContainer").style.width = newwidth;
+for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
 //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
 //var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-//document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-//}
+document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+}
 }
 changePizzaSizes(size);
 // User Timing API is awesome

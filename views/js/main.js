f5e55fcba5122aa2d5cb562e3e40b23955a4677d
edwin-github <edwin-github@users.noreply.github.com>
2015-03-18 15:46:08 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -416,7 +416,7 @@ for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i
 //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
 //var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
 //console.log("newwidth: " + newwidth);
-document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
 }
 }
 changePizzaSizes(size);
